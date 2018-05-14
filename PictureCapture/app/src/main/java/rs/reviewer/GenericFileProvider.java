90d06fe38f600ed86dcff3682722106b90a58d2c
milossimic <milossimicsimo@gmail.com>
2018-05-14 14:22:57 +0200

    new example

---
+++ PictureCapture/app/src/main/java/rs/reviewer/GenericFileProvider.java
@@ -0,0 +1,13 @@
+package rs.reviewer;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by milossimic on 10/28/17.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+    public static final String MY_PROVIDER = "rs.reviewer.provider";
+
+}
